2511ce01793d2abe83e641983ac439c6374ff05c
alumno <alumno>
2019-03-27 09:37:52 -0300

    9.37

---
+++ clase 4/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a;
+    float b;
+
+    printf("Elija un numero");
+    scanf("%d", &a);
+
+    printf ("Elija el segundo numero");
+    scanf("%d", &b);
+
+
+
+    return 0;
+}
