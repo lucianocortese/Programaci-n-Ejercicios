13fe15ee7ab65cca01e263f8cf64fc70fd284495
alumno <alumno@LAB00PC00>
2019-05-20 12:26:33 -0300

    dgsadg

--- Clase 17/Mem dinamica/Mem dinamica estruct/main.c
+++ Clase 17/Mem dinamica/Mem dinamica estruct/main.c
@@ -12,6 +12,10 @@ float sueldo;
 }eEmpleado;
 
 
+eEmpleado* new_Empleado();
+eEmpleado* new_EmpleadoParam(int leg, char* nombre, char sexo, float sueldo);
+
+
 int main()
 {
 
@@ -43,5 +47,73 @@ int main()
 
 
     printf("%d    %s    %c    %.2f", pEmp->legajo, pEmp->nombre, pEmp->sexo, pEmp->sueldo);
+
+
+
+
+
+
+
+
+
+    ///////////////////////////
+
+    eEmpleado* unEmpleado;
+    eEmpleado* otroEmpleado;
+
+    unEmpleado=new_Empleado();
+    otroEmpleado=new_EmpleadoParam(222,"lucho", 'n', 40000);
+
+    mostrarEmpleado(unEmpleado);
+    mostrarEmpleado(otroEmpleado);
+
+
+
     return 0;
 }
+
+eEmpleado* new_Empleado(){
+
+    eEmpleado* nuevo;
+
+    nuevo=(eEmpleado*) malloc (sizeof(eEmpleado));
+
+    if(nuevo!=NULL){
+
+        nuevo->legajo=0;
+        strcpy(nuevo->nombre,"");
+        nuevo->sexo=' ';
+        nuevo->sueldo=0;
+
+
+    }
+
+    return nuevo;
+}
+
+void mostrarEmpleado(eEmpleado* empleado){
+
+        if (empleado!=NULL){
+
+            printf("%d    %s    %c    %.2f", empleado->legajo, empleado->nombre, empleado->sexo, empleado->sueldo);
+
+        }
+
+}
+
+
+eEmpleado* new_EmpleadoParam(int leg, char* nombre, char sexo, float sueldo){
+
+    eEmpleado* nuevo;
+
+    nuevo=(eEmpleado*) malloc (sizeof(eEmpleado));
+
+    if(nuevo!=NULL){
+
+        nuevo->legajo=leg;
+        strcpy(nuevo->nombre,nombre);
+        nuevo->sexo=sexo;
+        nuevo->sueldo=sueldo;
+    }
+
+    }
