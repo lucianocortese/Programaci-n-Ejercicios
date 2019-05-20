d48af669f819c9a4b89e2481b265369a2c0baf5b
alumno <alumno@LAB00PC00>
2019-05-20 12:08:10 -0300

    sdgsdg

--- Clase 17/Mem dinamica/Mem dinamica estruct/main.c
+++ Clase 17/Mem dinamica/Mem dinamica estruct/main.c
@@ -14,6 +14,34 @@ float sueldo;
 
 int main()
 {
-    printf("Hello world!\n");
+
+    eEmpleado emp1;
+
+    eEmpleado* pEmp = (eEmpleado*)malloc(sizeof(eEmpleado));
+
+    if(pEmp==NULL){
+        exit(1);
+    }
+
+    printf("Ingrese legajo: ");
+    //Usas el & porque legajo es de tipo int a pesar de llegar a el por un puntero. (POR ESO EL OPERADOR FECHA)
+    scanf("%d", &pEmp->legajo);
+
+    printf("Ingrese nombre: ");
+    //Aca no porque es un char ya es la dirección de memoria.
+    setbuf(stdin, NULL);
+    //gets (pemp->nombre);
+    scanf("%s", pEmp->nombre);
+
+    printf("Ingrese sexo: ");
+    setbuf(stdin, NULL);
+    scanf("%c", &pEmp->sexo);
+
+    printf("Ingrese sueldo: ");
+    scanf("%f", &pEmp->sueldo);
+
+
+
+    printf("%d    %s    %c    %.2f", pEmp->legajo, pEmp->nombre, pEmp->sexo, pEmp->sueldo);
     return 0;
 }
